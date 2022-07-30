e1bf4d9df5f836a08a468fe0e6895ee65ca0a112
DESKTOP-Q70ONVS\akrik <sonyusasha@gmail.com>
2022-07-30 15:12:31 +0500

    open url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\tools\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com/");
+
+    }
 }
